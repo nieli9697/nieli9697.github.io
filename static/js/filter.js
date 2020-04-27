92c7737340a9e257189127b9b4876b0a6d2de1c9
nieli9697 <nieli9697@gmail.com>
2020-04-27 00:18:27 -0500

    adding content

--- static/js/filter.js
+++ static/js/filter.js
@@ -1,10 +1,44 @@
 $('#filter').on( 'click', 'a', function() {
 
+    document.getElementById("proj_9").style.display = "block";
+    document.getElementById("proj_8").style.display = "block";
+    document.getElementById("proj_7").style.display = "block";
+    document.getElementById("proj_6").style.display = "block";
+    document.getElementById("proj_5").style.display = "block";
+    document.getElementById("proj_4").style.display = "block";
+    document.getElementById("proj_3").style.display = "block";
     document.getElementById("proj_2").style.display = "block";
     document.getElementById("proj_1").style.display = "block";
     document.getElementById("proj_0").style.display = "block";
 
     var filterValue = $(this).attr('data-filter');
+
+    if(!$("#proj_9").hasClass(filterValue)){
+        document.getElementById("proj_9").style.display = "none";
+    }
+
+    if(!$("#proj_8").hasClass(filterValue)){
+        document.getElementById("proj_8").style.display = "none";
+    }
+
+    if(!$("#proj_7").hasClass(filterValue)){
+        document.getElementById("proj_7").style.display = "none";
+    }
+    if(!$("#proj_6").hasClass(filterValue)){
+        document.getElementById("proj_6").style.display = "none";
+    }
+
+    if(!$("#proj_5").hasClass(filterValue)){
+        document.getElementById("proj_5").style.display = "none";
+    }
+
+    if(!$("#proj_4").hasClass(filterValue)){
+        document.getElementById("proj_4").style.display = "none";
+    }
+
+    if(!$("#proj_3").hasClass(filterValue)){
+        document.getElementById("proj_3").style.display = "none";
+    }
 
     if(!$("#proj_2").hasClass(filterValue)){
         document.getElementById("proj_2").style.display = "none";
@@ -17,6 +51,5 @@ $('#filter').on( 'click', 'a', function() {
     if(!$("#proj_1").hasClass(filterValue)){
         document.getElementById("proj_1").style.display = "none";
     }
-
 
 });
